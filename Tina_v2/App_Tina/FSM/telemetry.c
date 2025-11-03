7868b6e95af9db6a0f620b0bfa1bb22c3fd39640
NFJJunior <iancu_ivasciuc@yahoo.com>
2025-11-03 01:13:34 +0100

    fix: send -1 for pres and alt when sensor fails

--- Tina_v2/App_Tina/FSM/telemetry.c
+++ Tina_v2/App_Tina/FSM/telemetry.c
@@ -19,8 +19,15 @@ void telemetry_send(SensorData_t sensor_data, float altitude, uint8_t fsm_state)
 	telemetry.gyro_x = (int16_t)sensor_data.gyro_x;
 	telemetry.gyro_y = (int16_t)sensor_data.gyro_y;
 	telemetry.gyro_z = (int16_t)sensor_data.gyro_z;
-	telemetry.pressure = (int16_t)(sensor_data.pres / 100); // hPa
-	telemetry.altitude = (int16_t)altitude;
+
+	if (sensor_data.pres > 0.0) {
+		telemetry.pressure = (int16_t)(sensor_data.pres / 100);
+		telemetry.altitude = (int16_t)altitude;
+	} else {
+		telemetry.pressure = (int16_t)sensor_data.pres;
+		telemetry.altitude = -1;
+	}
+
 	telemetry.fsm_state = fsm_state;
 
 	radio_send_telemetry_packet(telemetry);
